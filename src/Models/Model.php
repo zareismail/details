23744391c07782721a8077598c18f839415b6e03
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-11-19 18:47:41 +0330

    Fixes not exists config method

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -4,13 +4,21 @@ namespace Zareismail\Details\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Model as LaravelModel, SoftDeletes};  
-use Armincms\Targomaan\Concerns\InteractsWithTargomaan;
-use Zareismail\NovaContracts\Auth\Authorization;
-use Zareismail\NovaContracts\Auth\Authorizable;  
+use Armincms\Targomaan\Concerns\InteractsWithTargomaan; 
 
-class Model extends LaravelModel/* implements Authorizable*/
-{
-	use InteractsWithTargomaan; 
-    use HasFactory, SoftDeletes; 
-    // use Authorization; 
+class Model extends LaravelModel 
+{ 
+    use HasFactory, SoftDeletes, InteractsWithTargomaan;  
+
+    /**
+     * Get the config value with the given key.
+     * 
+     * @param  string $key    
+     * @param  mixed $default
+     * @return mixed         
+     */
+    public function config(string $key, $default = null)
+    {
+    	return data_get($this->config, $key, $default);
+    }
 }
